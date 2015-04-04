ae2f24379986fcd97637236a08049ee1a2ddccf0
stefano <stethewwolf@gmail.com>
2015-04-04 18:53:05 +0200

    testing servo and ultrasonic sensor DONE

--- uilli/uilli.ino
+++ uilli/uilli.ino
@@ -1,13 +1,82 @@
 #include <Servo.h>
+/*
+   DEFINITIONS
+*/
 
+/* 
+   Servo manager
+*/
+#define PIN_SERVO A0
+#define START_POS_SERVO 90
+Servo sensorServo;
+
+void servo_setup(){
+  pinMode(PIN_SERVO,OUTPUT);
+  sensorServo.attach(PIN_SERVO);
+  sensorServo.write(START_POS_SERVO);  
+  return;
+}
+
+void servo_move(int pos){
+  if(pos>=0 && pos <= 180 )
+    sensorServo.write(pos);
+  return;
+}
+
+void servo_reset(){
+  sensorServo.write(START_POS_SERVO);
+}
+
+/*
+   Ultrasonic Sensor
+*/
+#define USONIC_ECHO 13
+#define USONIC_TRIG 12
+int uSensorStat;
+
+void uSonic_setup(){
+  pinMode(USONIC_ECHO,INPUT);
+  pinMode(USONIC_TRIG,OUTPUT);
+
+  uSensorStat=0;
+}
+
+long uSonic_read(){
+  long duration, distance;
+      
+  digitalWrite(USONIC_TRIG,LOW);
+  delayMicroseconds(2);
+  digitalWrite(USONIC_TRIG,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(USONIC_TRIG,LOW);
+  duration = pulseIn(USONIC_ECHO,HIGH);
+  distance = (duration / 2 ) / 38;
+	    
+  return distance;
+}
+
+
+
+/*
+*   Final functions
+*/
 void setup(void){
-  pinMode(13,OUTPUT);  
+  servo_setup();
+  uSonic_setup();
+  Serial.begin(9600);
 }
 
 void loop(void){
-  digitalWrite(13,HIGH);
-  delay(1000);
-  digitalWrite(13,LOW);
-  delay(1000);
+  int i;
+  long dist;
+  delay(500);
+  for(i=0; i<180; i=i+2){
+    servo_move(i);
+    dist=uSonic_read();
+    Serial.println(dist); 
+    delay(25);
+  }
+  
+  servo_reset();
 }
 
